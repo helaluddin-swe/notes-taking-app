6eecfa3c0af09ee0169b33b9b7a2bdac261fe435
Helal Uddin -SWE <seoguro1999@gmail.com>
2025-12-07 23:22:55 +0600

    update

--- frontend/src/utils/apiInstance.js
+++ frontend/src/utils/apiInstance.js
@@ -1,12 +1,24 @@
-import axios from "axios";
+import {axios} from "axios"
+const PRIMARY_URL = "https://destination-coder.onrender.com/api";
+const BACKUP_URL = "https://destination-coder-1.onrender.com/api";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://notes-taking-app-2-twkv.onrender.com/api";
+let BASE_URL = PRIMARY_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
+
+// Auto failover
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.code === "ERR_NETWORK" && BASE_URL !== BACKUP_URL) {
+      BASE_URL = BACKUP_URL;
+      api.defaults.baseURL = BACKUP_URL;
+      return api(error.config);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
